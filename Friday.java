c4ee638afff9ab13b33e44042cb2fba8fc2709d0
bmam111 <superstarhero66@126.com>
2017-11-10 18:44:01 +0800

    update

--- Friday.java
+++ Friday.java
@@ -2,10 +2,7 @@ public class Friday{
 	public static void main(String [] args){
 		System.out.println("Hello World!");	
 	}
-<<<<<<< HEAD
 	//这是branch
-=======
 	//测试中文
 	//星期五
->>>>>>> refs/remotes/origin/master
 }
